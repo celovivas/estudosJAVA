01fbdf6262f6a17772448389802b772b324e67b5
Marcelo <mvivasd@indracompany.com>
2019-04-08 10:34:34 -0300

    Início estudo TypeAnotations
    Início estudo TypeAnotations

---
+++ src/br/com/estudosJAVA/java8/ExemplosTypeAnnotation.java
@@ -0,0 +1,52 @@
+package br.com.estudosJAVA.java8;
+
+import java.sql.Connection;
+
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
+public class ExemplosTypeAnnotation {
+
+	public static void main(String[] args) {
+
+		ExemplosTypeAnnotation exemplo = new ExemplosTypeAnnotation();
+		
+		
+		try {
+			System.out.print("Somando com valor null: ");
+			System.out.println(exemplo.somar1(null, 20));	
+		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		System.out.println(exemplo.somar2(null, 20));
+		try {
+			System.out.print("Somando com valor null: ");
+			System.out.println(exemplo.somar2(null, 20));	
+		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+			e.printStackTrace();		}
+		
+
+	}
+	
+	
+	
+	public Integer somar1(Integer valor1, Integer valor2){
+		@Open Connection conn;
+		
+		return valor1 + valor2;
+		
+	}
+	
+	public Integer somar2(@NotNull Integer valor1,@Nullable Integer valor2){
+		
+		return valor1 + valor2;
+		
+	}	
+	
+}
+
+
+
